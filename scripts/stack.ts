dbf90e2edb3a8920b6ae99bd95e67aba2d0bf905
zioroboco <zioroboco@gmail.com>
2018-04-22 17:44:21 +1000

    Merge create/update-stack into a single script

---
+++ scripts/stack.ts
@@ -0,0 +1,29 @@
+import { CloudFormation } from "aws-sdk"
+import { readFileSync } from "fs"
+import { argv } from "yargs"
+
+const template = argv._[0]
+const createOpt = argv.create
+
+if (!template) {
+  console.error("Must specify a template!")
+  process.exit(2)
+}
+
+type Params = CloudFormation.CreateStackInput | CloudFormation.UpdateStackInput
+const params: Params = {
+  StackName: process.env.STACK_NAME || "stack",
+  TemplateBody: readFileSync(template).toString()
+}
+
+const operation = createOpt
+  ? (cf: CloudFormation, params: Params) => cf.createStack(params)
+  : (cf: CloudFormation, params: Params) => cf.updateStack(params)
+
+operation(new CloudFormation(), params)
+  .promise()
+  .then(response =>
+    console.log(
+      `Stack ${createOpt ? "create" : "update"}d with id=${response.StackId}`
+    )
+  )
